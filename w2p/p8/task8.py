33d575e6962f34478eceb4407dcb29bad8a4ef49
Tymoshenko Mykhailo <tymoshenkomykhailo@Tymoshenkos-MacBook-Air.local>
2026-03-22 00:19:10 +0200

    Completed task 8 (w2p)

--- w2p/p8/task8.py
+++ w2p/p8/task8.py
@@ -14,3 +14,35 @@
 #       must print an "Invalid Input" warning and keep the previous value.
 #     - **Output:** A method `generate_invoice()` that prints a professional 
 #       breakdown: Base pay, Overtime bonus, and Grand Total.
+
+class ServiceInvoice():
+    def __init__(self, rate):
+        self.__hours = 0
+        self.rate = rate
+
+    def set_hours(self, hours):
+        if hours >= 0:
+            self.__hours = hours
+        else:
+            print("Invalid Input")
+
+    def calculate_total(self):
+        if self.__hours >= 40:
+            return (40*self.rate)+(self.__hours-40)*self.rate*1.5
+        else:
+            return self.__hours*self.rate
+
+    def generate_invoice(self):
+        if self.__hours > 40:
+            overtime = self.__hours-40
+        else:
+            overtime = 0
+
+        total = self.calculate_total()
+        
+        print(f"{'Regular Hours:':<20}{self.__hours}")
+        print(f"{'Overtime hours':<20}{overtime}")
+        print(f"{'Base pay:':<20}{40*self.rate}")
+        print(f"{'Overtime Bonus:':<20}{overtime*(self.rate * 1.5)}")
+        print(f"{'Total:':<20}{total}")
+        
