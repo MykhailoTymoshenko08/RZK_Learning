d4a0f8a9783f40dd1f5b4b1b0737a60c291d53c4
Tymoshenko Mykhailo <tymoshenkomykhailo@Tymoshenkos-MacBook-Air.local>
2026-04-02 13:31:21 +0300

    init w3t and adding theory for the third week

---
+++ w3p/task1.py
@@ -0,0 +1,14 @@
+# - HR Analytics Dashboard (Pandas)
+#     - **Essence:** Analyzing employee performance and salary gaps.
+#     - **TT:** Create a DataFrame from a dictionary of 50 employees 
+#       (Name, Department, Salary, Performance Score 1-10).
+#     - **Requirements:** 
+#       1. Filter employees with high performance but below-average salary in their department.
+#       2. Group by "Department" to find the total budget and average performance.
+
+import pandas as pd
+
+dataset = pd.DataFrame(
+    {}
+)
+
